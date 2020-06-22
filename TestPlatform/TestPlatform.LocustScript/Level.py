f5348eae0561c427256fdfc252f51ff0c61a5f6b
Hao Ren <rhw1111@163.com>
2020-06-22 13:46:33 +0800

    add label

---
+++ TestPlatform/TestPlatform.LocustScript/Level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+i1=int(input(u"成绩："))
+
+if i1>=60:
+    print(u"合格")
+else:
+    print(u"不合格")
+
+
+
---
+++ TestPlatform/TestPlatform.LocustScript/Score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+i1=int(input(u"请输入语文分数："))
+i2=int(input(u"请输入数学分数："))
+i3=int(input(u"请输入英语分数："))
+
+print(u"平均成绩是："+str((i1+i2+i3)/3))
+
+
