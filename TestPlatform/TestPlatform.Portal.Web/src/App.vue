14ee717ede922b10940a10bc7b764c636750b71b
a-liyao <871811594@qq.com>
2020-06-25 18:09:36 +0800

    增加testcase和tesedatasource页面

--- TestPlatform/TestPlatform.Portal.Web/src/App.vue
+++ TestPlatform/TestPlatform.Portal.Web/src/App.vue
@@ -18,4 +18,8 @@ export default {
 </script>
 
 <style>
+html,
+body {
+  height: 100%;
+}
 </style>
---
+++ TestPlatform/TestPlatform.Portal.Web/src/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="testcase">
+    <q-layout view="hHh lpR fFf">
+
+      <q-header elevated
+                class="bg-primary text-white"
+                height-hint="98">
+        <q-toolbar>
+          <q-toolbar-title>
+            <!-- <q-avatar>
+              <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+            </q-avatar> -->
+            test
+          </q-toolbar-title>
+        </q-toolbar>
+
+        <q-tabs align="left">
+          <q-route-tab to="/TestCase"
+                       label="TestCase" />
+          <q-route-tab to="/TestDataSource"
+                       label="TestDataSource" />
+        </q-tabs>
+      </q-header>
+
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+
+    </q-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'testcase',
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
