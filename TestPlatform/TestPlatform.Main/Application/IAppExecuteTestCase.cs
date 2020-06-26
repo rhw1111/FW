23da60a679b4358d0e7a054edd2469e2cbdae812
a-zhangjunhui <a-zhangjunhui@longhu.net>
2020-06-27 00:47:10 +0800

    修改TestCaseController

--- TestPlatform/TestPlatform.Main/Application/IAppExecuteTestCase.cs
+++ TestPlatform/TestPlatform.Main/Application/IAppExecuteTestCase.cs
@@ -14,12 +14,12 @@ namespace FW.TestPlatform.Main.Application
         Task<TestCaseViewData> Run(TestCaseAddModel model, CancellationToken cancellationToken = default);
         Task<TestCaseViewData> Stop(TestCaseAddModel model, CancellationToken cancellationToken = default);
         Task<TestCaseViewData> IsEngineRun(TestCaseAddModel model, CancellationToken cancellationToken = default);
-        Task AddSlaveHost(TestCase tCase, TestCaseSlaveHost slaveHost, CancellationToken cancellationToken = default);
+        Task AddSlaveHost(TestCaseSlaveHost slaveHost, CancellationToken cancellationToken = default);
         IAsyncEnumerable<TestCaseSlaveHost> GetAllSlaveHosts(TestCaseAddModel model, CancellationToken cancellationToken = default);
         Task DeleteSlaveHost(TestCase tCase, Guid slaveHostID, CancellationToken cancellationToken = default);
         Task<QueryResult<TestCaseHistory>> GetHistories(Guid caseID, int page, int pageSize, CancellationToken cancellationToken = default);
         Task<TestCaseHistory?> GetHistory(TestCase tCase, Guid historyID, CancellationToken cancellationToken = default);
         Task DeleteHistory(TestCase tCase, Guid historyID, CancellationToken cancellationToken = default);
-        Task UpdateSlaveHost(TestCase tCase, TestCaseSlaveHost slaveHost, CancellationToken cancellationToken = default);
+        Task UpdateSlaveHost(TestCaseSlaveHost slaveHost, CancellationToken cancellationToken = default);
     }
 }
