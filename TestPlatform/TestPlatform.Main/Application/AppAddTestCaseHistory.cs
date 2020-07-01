29d04bb9db891bf44ce940d27f0cbb4e4fb53895
Jason XIONG <jaxion@microsoft.com>
2020-07-01 16:59:12 +0800

    修改addhistory接口

--- TestPlatform/TestPlatform.Main/Application/AppAddTestCaseHistory.cs
+++ TestPlatform/TestPlatform.Main/Application/AppAddTestCaseHistory.cs
@@ -9,20 +9,43 @@ using FW.TestPlatform.Main.DTOModel;
 using FW.TestPlatform.Main.Entities;
 using System.Linq;
 using System.Diagnostics.Tracing;
+using MSLibrary.LanguageTranslate;
+using MSLibrary.Serializer;
 
 namespace FW.TestPlatform.Main.Application
 {
     [Injection(InterfaceType = typeof(IAppAddTestCaseHistory), Scope = InjectionScope.Singleton)]
     public class AppAddTestCaseHistory : IAppAddTestCaseHistory
     {
+        private readonly ITestCaseRepository _testCaseRepository;
+
+        public AppAddTestCaseHistory(ITestCaseRepository testCaseRepository)
+        {
+            _testCaseRepository = testCaseRepository;
+        }
+
         public async Task Do(TestCaseHistorySummyAddModel model, CancellationToken cancellationToken = default)
         {
-            TestCase source = new TestCase()
-            {
-                ID = model.CaseID
-            };
+            TestCase? testCase = await _testCaseRepository.QueryByID(model.CaseID);
 
-            await source.AddHistory(model);
+            if (testCase == null)
+            {
+                var fragment = new TextFragment()
+                {
+                    Code = TestPlatformTextCodes.NotFoundTestCaseByID,
+                    DefaultFormatting = "找不到ID为{0}的测试案例",
+                    ReplaceParameters = new List<object>() { model.CaseID }
+                };
+
+                throw new UtilityException((int)TestPlatformErrorCodes.NotFoundTestCaseByID, fragment, 1, 0);
+            }
+
+            TestCaseHistory history = new TestCaseHistory();
+            history.CaseID = model.CaseID;
+            //history.Case = testCase;                
+            history.Summary = JsonSerializerHelper.Serializer(model);
+
+            await testCase.AddHistory(history);
         }
     }
 }
