bb29cf1d00441648ccdc22ae47398194f47863a4
a-liyao <871811594@qq.com>
2020-07-02 04:27:53 +0800

    增加TestCase页面和接口

--- TestPlatform/TestPlatform.Portal.Web/src/main.js
+++ TestPlatform/TestPlatform.Portal.Web/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index'
 import './quasar'
-Vue.config.productionTip = true;
+window.VueInstance = Vue;
 
 new Vue({
   router,
