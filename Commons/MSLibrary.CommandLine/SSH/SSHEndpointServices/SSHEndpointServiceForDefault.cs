ef9176a6031e8c3301800fe14e66f64ee7ed87ed
Hao Ren <rhw1111@163.com>
2020-07-02 15:51:48 +0800

    fix

--- Commons/MSLibrary.CommandLine/SSH/SSHEndpointServices/SSHEndpointServiceForDefault.cs
+++ Commons/MSLibrary.CommandLine/SSH/SSHEndpointServices/SSHEndpointServiceForDefault.cs
@@ -52,7 +52,7 @@ namespace MSLibrary.CommandLine.SSH.SSHEndpointServices
             {
 
                 sshClient.Connect();
-                result = await sshClient.RunCommand(command).ExecuteAsync();
+                result = await sshClient.CreateCommand(command).ExecuteAsync();
 
                 sshClient.Disconnect();
             }
@@ -92,7 +92,7 @@ namespace MSLibrary.CommandLine.SSH.SSHEndpointServices
                 foreach (var item in commondGenerators)
                 {
                     var command = await item(result);
-                    result = await sshClient.RunCommand(command).ExecuteAsync();
+                    result = await sshClient.CreateCommand(command).ExecuteAsync();
                 }
 
                 sshClient.Disconnect();
@@ -187,7 +187,7 @@ namespace MSLibrary.CommandLine.SSH.SSHEndpointServices
         }
         public async Task<string> Do(string command)
         {
-            return await _sshClient.RunCommand(command).ExecuteAsync();
+            return await _sshClient.CreateCommand(command).ExecuteAsync();
         }
     }
 }
