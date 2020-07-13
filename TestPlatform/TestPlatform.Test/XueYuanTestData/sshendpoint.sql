9f57fcb5543e8fa9943bd73daa8b14c3e45c1f42
xueyuangithub <xueyuan@msn.com>
2020-07-13 18:06:44 +0800

    update sql

--- TestPlatform/TestPlatform.Test/XueYuanTestData/sshendpoint.sql
+++ TestPlatform/TestPlatform.Test/XueYuanTestData/sshendpoint.sql
@@ -1,12 +1,16 @@
 SELECT * FROM tpmain.sshendpoint;
 
-INSERT INTO tpmain.sshendpoint
+REPLACE INTO tpmain.sshendpoint
 VALUES('1b846704-5449-4585-bb15-8b13388cb68b', 'Default', 'name', '{
     "Address": "13.68.249.103",
     "Port": "22",
     "UserName": "TPUser",
     "Password": "Password01asd!"
+}', now(), now(), '1'),
+('3a80b443-c4eb-11ea-8951-00ffb1d16cf9', 'Default', 'name', '{
+    "Address": "20.185.245.250",
+    "Port": "22",
+    "UserName": "TPUser",
+    "Password": "Password01asd!"
 }', now(), now(), '2');
-
-
 
