72afe45b79d64bec0446d8ab5a141e34bd7e5855
xueyuangithub <xueyuan@msn.com>
2020-07-17 14:57:20 +0800

    add SplitJsonData

---
+++ TestPlatform/TestPlatform.Main/Code/GenerateAdditionFuncServices/GenerateAdditionFuncServiceForLocustSplitJsonData.cs
@@ -0,0 +1,42 @@
+﻿using MSLibrary.DI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FW.TestPlatform.Main.Code.GenerateAdditionFuncServices
+{
+    /// <summary>
+    /// 针对Locust的附件函数生成服务
+    /// </summary>
+    [Injection(InterfaceType = typeof(GenerateAdditionFuncServiceForLocustSplitJsonData), Scope = InjectionScope.Singleton)]
+    public class GenerateAdditionFuncServiceForLocustSplitJsonData : IGenerateAdditionFuncService
+    {
+        public async Task<string> Generate()
+        {
+            StringBuilder sbCode = new StringBuilder();
+            sbCode.AppendLine("def SplitJsonData(data, piece):");
+            sbCode.AppendLine("    # print(\"SplitJsonData\")");
+            sbCode.AppendLine("");
+            sbCode.AppendLine("    if data and type(data) == dict:");
+            sbCode.AppendLine("        return data");
+            sbCode.AppendLine("    elif piece <= 0:");
+            sbCode.AppendLine("        return None");
+            sbCode.AppendLine("    elif data and len(data) > 0:");
+            sbCode.AppendLine("        if len(data) < piece:");
+            sbCode.AppendLine("            return None");
+            sbCode.AppendLine("        else:");
+            sbCode.AppendLine("            length = len(data)");
+            sbCode.AppendLine("            i = length // piece");
+            sbCode.AppendLine("            result = data[0:i]");
+            sbCode.AppendLine("            del(data[0:i])");
+            sbCode.AppendLine("");
+            sbCode.AppendLine("            return result");
+            sbCode.AppendLine("        ");
+            sbCode.AppendLine("    return None");
+            sbCode.AppendLine("");
+
+            return await Task.FromResult(sbCode.ToString());
+        }
+    }
+}
--- TestPlatform/TestPlatform.Main/Template/LabelParameterHandlers/LabelParameterHandlerForSplitJsonDataInvoke.cs
+++ TestPlatform/TestPlatform.Main/Template/LabelParameterHandlers/LabelParameterHandlerForSplitJsonDataInvoke.cs
@@ -14,16 +14,16 @@ using System.Text.RegularExpressions;
 namespace FW.TestPlatform.Main.Template.LabelParameterHandlers
 {
     /// <summary>
-    ///过滤数据
-    ///格式:{$getjsondatainvoke(data,gettype,endtype)}
-    ///参数：1，Json字符串 2，0=顺序取值，1=唯一取值，2=随机取值 3，0=结束测试，1=循环使用，2=循环停留最后一个
-    ///返回值：过滤后数据
-    [Injection(InterfaceType = typeof(LabelParameterHandlerForGetJsonDataInvoke), Scope = InjectionScope.Singleton)]
-    public class LabelParameterHandlerForGetJsonDataInvoke : ILabelParameterHandler
+    ///计算校验和
+    ///格式:{$splitjsondatainvoke(data,piece)}
+    ///参数：1，Json字符串，2，份数
+    ///返回值：校验和
+    [Injection(InterfaceType = typeof(LabelParameterHandlerForSplitJsonDataInvoke), Scope = InjectionScope.Singleton)]
+    public class LabelParameterHandlerForSplitJsonDataInvoke : ILabelParameterHandler
     {
         private readonly ISelector<IFactory<IGetSeparatorService>> _getSeparatorServiceSelector;
 
-        public LabelParameterHandlerForGetJsonDataInvoke(ISelector<IFactory<IGetSeparatorService>> getSeparatorServiceSelector)
+        public LabelParameterHandlerForSplitJsonDataInvoke(ISelector<IFactory<IGetSeparatorService>> getSeparatorServiceSelector)
         {
             _getSeparatorServiceSelector = getSeparatorServiceSelector;
         }
@@ -32,13 +32,13 @@ namespace FW.TestPlatform.Main.Template.LabelParameterHandlers
         {
             StringBuilder strCode = new StringBuilder();
 
-            if (parameters.Length < 3)
+            if (parameters.Length < 2)
             {
                 var fragment = new TextFragment()
                 {
                     Code = TextCodes.LabelParameterCountError,
                     DefaultFormatting = "标签{0}要求的参数个数为{1}，而实际参数个数为{2}",
-                    ReplaceParameters = new List<object>() { "{$getjsondatainvoke(data,gettype,endtype)}", 3, parameters.Length }
+                    ReplaceParameters = new List<object>() { "{$splitjsondatainvoke(data,piece)}", 2, parameters.Length }
                 };
 
                 throw new UtilityException((int)Errors.LabelParameterCountError, fragment, 1, 0);
@@ -52,26 +52,13 @@ namespace FW.TestPlatform.Main.Template.LabelParameterHandlers
                 {
                     Code = TextCodes.LabelParameterTypeError,
                     DefaultFormatting = "标签{0}要求的参数{1}应为{2}，参数类型错误",
-                    ReplaceParameters = new List<object>() { "{$getjsondatainvoke(data,gettype,endtype)}", "gettype", "Int" }
+                    ReplaceParameters = new List<object>() { "{$splitjsondatainvoke(data,piece)}", "piece", "Int" }
                 };
 
                 throw new UtilityException((int)Errors.LabelParameterTypeError, fragment, 1, 0);
             }
 
-            if (!regex.IsMatch(parameters[2]))
-            {
-                var fragment = new TextFragment()
-                {
-                    Code = TextCodes.LabelParameterTypeError,
-                    DefaultFormatting = "标签{0}要求的参数{1}应为{2}，参数类型错误",
-                    ReplaceParameters = new List<object>() { "{$getjsondatainvoke(data,gettype,endtype)}", "endtype", "Int" }
-                };
-
-                throw new UtilityException((int)Errors.LabelParameterTypeError, fragment, 1, 0);
-            }
-
-
-            strCode.Append($"GetJsonData({parameters[0]}, {parameters[1]}, {parameters[2]})");
+            strCode.Append($"SplitJsonData({parameters[0]},{parameters[1]})");
 
             return strCode.ToString();
         }
