1bda0b7786d55ce4af9fda15c82aa3be6fa4368e
a-zhangjunhui <a-zhangjunhui@longhu.net>
2020-07-27 12:40:25 +0800

    添加获取标签公式得DTO实体

---
+++ TestPlatform/TestPlatform.Main/Application/AppQueryFormulas.cs
@@ -0,0 +1,35 @@
+﻿using FW.TestPlatform.Main.DTOModel;
+using MSLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MSLibrary.DI;
+using FW.TestPlatform.Main.Entities;
+using MSLibrary.Template;
+using MongoDB.Bson;
+
+namespace FW.TestPlatform.Main.Application
+{
+    [Injection(InterfaceType = typeof(IAppQueryTestCase), Scope = InjectionScope.Singleton)]
+    public class AppQueryFormulas : IAppQueryFormulas
+    {
+        private readonly ITestCaseRepository _testCaseRepository;
+
+        public AppQueryFormulas(ITestCaseRepository testCaseRepository)
+        {
+            _testCaseRepository = testCaseRepository;
+        }
+
+        public async Task<List<FormulasViewData>> Do(CancellationToken cancellationToken = default)
+        {
+            await _testCaseRepository.QueryByID(new Guid());
+
+            List<FormulasViewData> result = new List<FormulasViewData>();
+            
+
+            return result;
+        }
+    }
+}
