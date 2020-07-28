e7f6c27d3ca9807229af367403877b3a717e9d9d
a-zhangjunhui <a-zhangjunhui@longhu.net>
2020-07-28 16:33:48 +0800

    修改注入文件

--- TestPlatform/TestPlatform.Main/Application/AppQuerySingleTestCase.cs
+++ TestPlatform/TestPlatform.Main/Application/AppQuerySingleTestCase.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using MSLibrary;
 using MSLibrary.DI;
 using FW.TestPlatform.Main.Entities;
 using MSLibrary.LanguageTranslate;
