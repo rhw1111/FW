a7e4fa2252e452e4beda5b64caa54b68637c8db7
Hao Ren <rhw1111@163.com>
2020-07-29 16:22:36 +0800

    modify

--- Commons/MSLibrary/Template/TemplateMainService.cs
+++ Commons/MSLibrary/Template/TemplateMainService.cs
@@ -123,7 +123,7 @@ namespace MSLibrary.Template
 
                         if (dictResult.TryGetValue(match.Index, out string matchResult))
                         {
-                            return matchResult.Replace(@"\", @"\\").Replace(@"}", @"\}").Replace(@"$", @"\$").Replace(@",", @"\,");
+                            return matchResult;
                         }
                         else
                         {
@@ -176,7 +176,7 @@ namespace MSLibrary.Template
             // 去空格
             for (int i = 0; i < arrayParamaters.Length; i++)
             {
-                arrayParamaters[i] = arrayParamaters[i].Trim().Replace(@"\}", "}").Replace(@"\$", "$").Replace(@"\,", ",").Replace(@"\\", @"\");
+                arrayParamaters[i] = arrayParamaters[i].Trim();
             }
 
             var parameter = await _labelParameterRepository.QueryByName(strLabelName);
