5bfa2b79ab08f1e5f9a14b870e0b83f5be534acc
Hao Ren <rhw1111@163.com>
2020-08-08 21:56:51 +0800

    add comment

--- TestPlatform/TestPlatform.Main/Entities/NetGateway/INetGatewayDataHandleService.cs
+++ TestPlatform/TestPlatform.Main/Entities/NetGateway/INetGatewayDataHandleService.cs
@@ -7,36 +7,76 @@ using System.Threading.Tasks;
 
 namespace FW.TestPlatform.Main.NetGateway
 {
+    /// <summary>
+    /// 网关数据处理服务
+    /// </summary>
     public interface INetGatewayDataHandleService
     {
         Task<INetGatewayDataHandleResult> Execute(CancellationToken cancellationToken = default);
     }
 
+    /// <summary>
+    /// 网关数据处理服务的配置信息获取服务
+    /// 涉及到要获取的配置信息，从该接口的方法获取
+    /// </summary>
     public interface INetGatewayDataHandleConfigurationService
     {
+        /// <summary>
+        /// 获取要监控处理的文件目录
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task<string> GetDataFileFolderPath(CancellationToken cancellationToken = default);
     }
 
+    /// <summary>
+    /// 从文件流中获取源数据字符串服务
+    /// 一个文件流中有多个源数据字符串
+    /// sourceDataAction针对每个源数据字符串做处理
+    /// </summary>
     public interface IGetSourceDataFromStreamService
     {
+        /// <summary>
+        /// 获取源数据字符串
+        /// sourceDataAction中可以得到从流中获取的每个源数据字符串
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="sourceDataAction"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         Task Get(Stream stream,Func<string,Task> sourceDataAction, CancellationToken cancellationToken = default);
     }
 
+    /// <summary>
+    /// 获取文件名称的前缀
+    /// 在当前场景中，该前缀为TestCaseHistory的ID
+    /// </summary>
     public interface IResolveFileNamePrefixService
     {
         string Resolve(string fileName);
     }
 
+    /// <summary>
+    /// 源数据字符串转换为自定义的网络数据
+    /// 自定义的网络数据用于计算各种指标
+    /// </summary>
     public interface IConvertNetDataFromSourceService
     {
         Task<NetData> Convert(string sourceData, CancellationToken cancellationToken = default);
     }
 
+    /// <summary>
+    /// QPS指标收集服务
+    /// 当前场景中，将传递该指标到流式数据库
+    /// </summary>
     public interface IQPSCollectService
     {
         Task Collect(int qps,DateTime time, CancellationToken cancellationToken = default);
     }
-
+    /// <summary>
+    /// 延迟指标收集服务
+    ///  当前场景中，将传递该指标到流式数据库
+    /// </summary>
     public interface INetDurationCollectService
     {
         Task Collect(double min,double max,double avg, DateTime time, CancellationToken cancellationToken = default);
