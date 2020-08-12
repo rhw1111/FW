77471c528b067b3ae84a2b60d28b055d0cceb776
Hao Ren <rhw1111@163.com>
2020-08-12 11:38:12 +0800

    add dockerfile-compose

--- TestPlatform/TestPlatform.Main/Schedule/Actions/ScheduleActionServiceForNetGateway.cs
+++ TestPlatform/TestPlatform.Main/Schedule/Actions/ScheduleActionServiceForNetGateway.cs
@@ -4,16 +4,40 @@ using System.Text;
 using System.Threading.Tasks;
 using MSLibrary.DI;
 using MSLibrary.Schedule;
+using FW.TestPlatform.Main.NetGateway;
 
 namespace FW.TestPlatform.Main.Schedule.Actions
 {
     [Injection(InterfaceType = typeof(ScheduleActionServiceForNetGateway), Scope = InjectionScope.Singleton)]
     public class ScheduleActionServiceForNetGateway : IScheduleActionService
     {
+        private readonly INetGatewayDataHandleService _netGatewayDataHandleService;
+
+        public ScheduleActionServiceForNetGateway(INetGatewayDataHandleService netGatewayDataHandleService)
+        {
+            _netGatewayDataHandleService = netGatewayDataHandleService;
+        }
         public async Task<IScheduleActionResult> Execute(string configuration)
         {
-            
-            return await Task.FromResult(new ScheduleActionResultDefault());
+            var handleResult=await _netGatewayDataHandleService.Execute();
+            return new ScheduleActionResult(handleResult);
+        }
+
+        private class ScheduleActionResult : IScheduleActionResult
+        {
+            private readonly INetGatewayDataHandleResult _netGatewayDataHandleResult;
+
+            public ScheduleActionResult(INetGatewayDataHandleResult netGatewayDataHandleResult)
+            {
+                _netGatewayDataHandleResult = netGatewayDataHandleResult;
+                Polling = true;
+            }
+            public bool Polling { get; private set; }
+
+            public async Task Stop()
+            {
+                await _netGatewayDataHandleResult.Stop();
+            }
         }
     }
 }
