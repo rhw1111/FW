bcb640c1447e73115ae7a470acb9c5dd873e9303
Hao Ren <rhw1111@163.com>
2020-08-12 10:19:19 +0800

    modify sql

--- TestPlatform/TestPlatform.Test/RenHaoTest.cs
+++ TestPlatform/TestPlatform.Test/RenHaoTest.cs
@@ -241,7 +241,7 @@ namespace TestPlatform.Test
         [Test]
         public async Task TestSchedule()
         {
-            var n=typeof(FW.TestPlatform.Main.Schedule.Actions.ScheduleActionServiceForTestFactory).AssemblyQualifiedName;
+            var n=typeof(FW.TestPlatform.Main.Schedule.Actions.ScheduleActionServiceForNetGatewayFactory).AssemblyQualifiedName;
             var id=Guid.NewGuid();
             var testDataSourceStore = DIContainerContainer.Get<IScheduleActionGroupStore>();
             var result=await testDataSourceStore.QueryByPage("", 1, 1);
