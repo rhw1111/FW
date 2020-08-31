b1ebe40b8c68ecfaee0cf1e4e8fe06151ead074f
a-zhangjunhui <a-zhangjunhui@longhu.net>
2020-08-31 18:46:53 +0800

    TreeEntity查询孩子列表

---
+++ TestPlatform/TestPlatform.Main/Application/AppQueryTreeEntityChildren.cs
@@ -0,0 +1,54 @@
+﻿using FW.TestPlatform.Main.DTOModel;
+using MSLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MSLibrary.DI;
+using FW.TestPlatform.Main.Entities;
+using MSLibrary.Collections;
+
+namespace FW.TestPlatform.Main.Application
+{
+    [Injection(InterfaceType = typeof(IAppQueryTreeEntityChildren), Scope = InjectionScope.Singleton)]
+    public class AppQueryTreeEntityChildren : IAppQueryTreeEntityChildren
+    {
+        private readonly ITreeEntityRepository _treeEntityRepository;
+
+        public AppQueryTreeEntityChildren(ITreeEntityRepository treeEntityRepository)
+        {
+            _treeEntityRepository = treeEntityRepository;
+        }
+
+        public async Task<QueryResult<TreeEntityViewModel>> Do(Guid? parentId, string? matchName, int? type, int page, int pageSize, CancellationToken cancellationToken = default)
+        {
+            QueryResult<TreeEntityViewModel> result = new QueryResult<TreeEntityViewModel>();
+            QueryResult<TreeEntity> queryResult = new QueryResult<TreeEntity>();
+            if (parentId == null)
+                await _treeEntityRepository.QueryRoot(matchName, type, page, pageSize, cancellationToken);
+            else
+                await _treeEntityRepository.QueryChildren((Guid)parentId, matchName, type, page, pageSize, cancellationToken);
+            //var queryResult = await _treeEntityRepository.QueryChildren(matchName, type, page, pageSize, cancellationToken);
+
+            result.CurrentPage = queryResult.CurrentPage;
+            result.TotalCount = queryResult.TotalCount;
+
+            foreach(var item in queryResult.Results)
+            {
+                result.Results.Add(
+                    new TreeEntityViewModel()
+                    {
+                        ID = item.ID,
+                        Name = item.Name,
+                        Type = item.Type,
+                        Value = item.Value,
+                        CreateTime = item.CreateTime
+                    }
+                    );
+            }
+
+            return result;
+        }
+    }
+}
