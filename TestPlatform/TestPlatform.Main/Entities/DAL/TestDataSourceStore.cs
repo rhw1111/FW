9a46432e272eecf7235705608a3b034c9fc472df
a-zhangjunhui <a-zhangjunhui@longhu.net>
2020-09-23 15:14:13 +0800

    修改把treeid等于null的属于parentid为null的目录

--- TestPlatform/TestPlatform.Main/Entities/DAL/TestDataSourceStore.cs
+++ TestPlatform/TestPlatform.Main/Entities/DAL/TestDataSourceStore.cs
@@ -193,7 +193,7 @@ namespace FW.TestPlatform.Main.Entities.DAL
                                     where item.ParentID == parentId && item.Type == 3
                                     select item.ID).CountAsync();
 
-                    result.TotalCount = count;
+                    //result.TotalCount = count;
                     var countWithoutTree = 0;
                     if (parentId == null)
                     {
@@ -202,35 +202,35 @@ namespace FW.TestPlatform.Main.Entities.DAL
                                                     select item).CountAsync();
                     }
                     result.TotalCount = count + countWithoutTree;
-                    int skipCount = 0;
-                    int takeCount = 0;
                     List<TestDataSource> datasWithoutTree = new List<TestDataSource>();
-                    if (countWithoutTree >= page * pageSize)
+                    if ((page - 1) * pageSize < countWithoutTree)
                     {
                         datasWithoutTree = await (from item in dbContext.TestDataSources
                                                   where item.TreeID == null
                                                   select item).Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
                     }
-                    else if ((page - 1) * pageSize <= countWithoutTree && countWithoutTree < page * pageSize)
+                    if (countWithoutTree < page* pageSize)
                     {
-                        skipCount = countWithoutTree - (page - 1) * pageSize;
-                        takeCount = page * pageSize - countWithoutTree;
-                        datasWithoutTree = await (from item in dbContext.TestDataSources
-                                                  where item.TreeID == null
-                                                  select item).Skip((page - 1) * pageSize).Take(pageSize).ToListAsync();
-                    }
-                    else
-                    {
-                        skipCount = (((page - 1) * pageSize - countWithoutTree) / pageSize) * pageSize;
-                        takeCount = pageSize - ((page - 1) * pageSize - countWithoutTree) % pageSize;
-                    }
-                    if (countWithoutTree < page * pageSize)
-                    {
+                        int skipCount = 0;
+                        int takeCount = 0;
+                        if (countWithoutTree <= (page - 1) * pageSize)
+                        {
+                            //skipCount = ((page - 1) * pageSize - countWithoutTree) <= pageSize ? (page - 1) * pageSize - countWithoutTree : (page - 1) * pageSize - countWithoutTree;
+                            //takeCount = ((page - 1) * pageSize - countWithoutTree) <= pageSize ? (pageSize - ((page - 1) * pageSize - countWithoutTree) % pageSize) : pageSize;
+                            skipCount = (page - 1) * pageSize - countWithoutTree;
+                            takeCount = pageSize;
+                        }
+                        else
+                        {
+                            skipCount = 0;
+                            takeCount = pageSize - (countWithoutTree - (page - 1) * pageSize);
+                        }
+
                         var ids = (from item in dbContext.TreeEntities
                                    where item.ParentID == parentId && item.Type == 3
                                    orderby EF.Property<long>(item, "Sequence") descending
                                    select item.Value
-                                        ).Skip((page - 1) * pageSize).Take(pageSize);
+                                        ).Skip(skipCount).Take(takeCount);
 
                         if (ids.Count() > 0)
                         {
@@ -246,15 +246,6 @@ namespace FW.TestPlatform.Main.Entities.DAL
                     {
                         result.Results.AddRange(datasWithoutTree);
                     }
-                    //if (parentId == null && page == 1)
-                    //{
-                    //    var datasWithoutTree = await (from item in dbContext.TestDataSources
-                    //                                  where item.TreeID == null
-                    //                                  orderby EF.Property<long>(item, "Sequence") descending
-                    //                                  select item).ToListAsync();
-                    //    if (datasWithoutTree != null)
-                    //        result.Results.AddRange(datasWithoutTree);
-                    //}
                 }
             });
 
