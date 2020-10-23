f848f771ed95382f3ded7b0e0da7e1c613dc72fe
xueyuangithub <xueyuan@msn.com>
2020-10-23 18:23:46 +0800

    udpate jmeter

--- TestPlatform/TestPlatform.Test/XueYuanTestData/scripttemplate-jmeter.sql
+++ TestPlatform/TestPlatform.Test/XueYuanTestData/scripttemplate-jmeter.sql
@@ -303,7 +303,7 @@ SET content = '      <PostThreadGroup guiclass="PostThreadGroupGui" testclass="P
             <connectTime>true</connectTime>
           </value>
         </objProp>
-        <stringProp name="filename">{Result}</stringProp>
+        <stringProp name="filename">{ResultStatVisualizer}</stringProp>
       </ResultCollector>
       <hashTree/>'
 WHERE id = '2b5931aa-1375-11eb-bbfc-00ffb1d16cf9';
