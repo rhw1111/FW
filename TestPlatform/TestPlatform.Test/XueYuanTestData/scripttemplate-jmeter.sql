a24725dadcc3f87ca16bcb3a250aa5d0205fee99
xueyuangithub <xueyuan@msn.com>
2020-10-28 16:02:29 +0800

    update jmeter

--- TestPlatform/TestPlatform.Test/XueYuanTestData/scripttemplate-jmeter.sql
+++ TestPlatform/TestPlatform.Test/XueYuanTestData/scripttemplate-jmeter.sql
@@ -53,7 +53,7 @@ SET content = '      <PostThreadGroup guiclass="PostThreadGroupGui" testclass="P
           <stringProp name="HTTPSampler.port">{CaseServiceBasePort}</stringProp>
           <stringProp name="HTTPSampler.protocol">http</stringProp>
           <stringProp name="HTTPSampler.contentEncoding">utf-8</stringProp>
-          <stringProp name="HTTPSampler.path">/api/report/addhistory</stringProp>
+          <stringProp name="HTTPSampler.path">/api/report/addhistoryforjmeter</stringProp>
           <stringProp name="HTTPSampler.method">POST</stringProp>
           <boolProp name="HTTPSampler.follow_redirects">true</boolProp>
           <boolProp name="HTTPSampler.auto_redirects">false</boolProp>
@@ -194,7 +194,7 @@ SET content = '      <PostThreadGroup guiclass="PostThreadGroupGui" testclass="P
             </elementProp>
             <elementProp name="application" elementType="Argument">
               <stringProp name="Argument.name">application</stringProp>
-              <stringProp name="Argument.value">{CaseID}</stringProp>
+              <stringProp name="Argument.value">{CaseID}:{CaseHistoryID}</stringProp>
               <stringProp name="Argument.metadata">=</stringProp>
             </elementProp>
             <elementProp name="measurement" elementType="Argument">
